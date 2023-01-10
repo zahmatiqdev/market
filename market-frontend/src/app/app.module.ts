17e757fb81600fdb468b8d156f1192d6212146f4
MohammadJavad <zahmati.mohammadjavad@gmail.com>
2023-01-10 16:19:28 +0330

    add config of HttpClientModule

--- market-frontend/src/app/app.module.ts
+++ market-frontend/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AccountComponent } from './account/account.component';
+import { SignUpComponent } from './account/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AccountComponent,
+    SignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

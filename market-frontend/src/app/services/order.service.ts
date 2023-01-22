b5ec3e82f72288c0fd8c0a09dad05c1d25c17006
MohammadJavad <zahmati.mohammadjavad@gmail.com>
2023-01-22 22:42:04 +0330

    add order service

---
+++ market-frontend/src/app/services/order.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Order } from "../models/order.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  baseURL: string = "http://127.0.0.1:8000/";
+
+  constructor(private http: HttpClient) { }
+
+  postCreateOrderRequest(order:Order): Observable<any>{
+    const headers = { 'content-type': 'application/json' };
+    const body=JSON.stringify(order);
+    return this.http.post(this.baseURL + 'api/market/order/create/', body, {'headers':headers})
+  }
+
+  getListOrderRequest(): Observable<any>{
+    const headers = { 'content-type': 'application/json' };
+    return this.http.get(this.baseURL + 'api/market/order/', {'headers':headers})
+  }
+
+  getRetrieveOrderRequest(id: string): Observable<any>{
+    const headers = { 'content-type': 'application/json' };
+    return this.http.get(this.baseURL + 'api/market/order/' + id + '/', {'headers':headers})
+  }
+}

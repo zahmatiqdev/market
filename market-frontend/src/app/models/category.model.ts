d14099a3969690c022598f663b4e591bb2a90f4f
MohammadJavad <zahmati.mohammadjavad@gmail.com>
2023-01-31 08:56:09 +0330

    add category & unit model

---
+++ market-frontend/src/app/models/category.model.ts
@@ -0,0 +1,10 @@
+export class Category {
+    public id: number;
+    public name: string;
+  
+    constructor(id: number, name: string) {
+      this.id = id;
+      this.name = name;
+    }
+  }
+  

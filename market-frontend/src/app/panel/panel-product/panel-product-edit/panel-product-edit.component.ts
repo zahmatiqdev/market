a6d38b3650ce0c79f9c6d2b315f067e3bec47342
MohammadJavad <zahmati.mohammadjavad@gmail.com>
2023-02-03 13:04:28 +0330

    fix category & unit - in edit product Component - to load edited data

--- market-frontend/src/app/panel/panel-product/panel-product-edit/panel-product-edit.component.ts
+++ market-frontend/src/app/panel/panel-product/panel-product-edit/panel-product-edit.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
+import { Unit } from 'src/app/models/unit.model';
+import { Category } from 'src/app/models/category.model';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -15,12 +17,16 @@ export class PanelProductEditComponent implements OnInit {
   id: number;
   editMode = true;
   productForm: FormGroup;
+  units: Unit[];
+  categories: Category[];
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute,
               private router: Router) {}
 
   ngOnInit() {
+    this.onGetListCategory();
+    this.onGetListUnit();
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
@@ -40,6 +46,8 @@ export class PanelProductEditComponent implements OnInit {
   }
 
   private initForm() {
+    let productCategory = [];
+    let productUnit = '';
     let productName = '';
     let productPrice = 0;
     let productShortDesc = '';
@@ -47,6 +55,8 @@ export class PanelProductEditComponent implements OnInit {
 
     if (this.editMode) {
       const product = this.productService.getProduct(+this.id);
+      productCategory = product.category;
+      productUnit = product.unit;
       productName = product.name;
       productPrice = product.price;
       productShortDesc = product.short_desc;
@@ -54,10 +64,28 @@ export class PanelProductEditComponent implements OnInit {
     }
 
     this.productForm = new FormGroup({
+      category: new FormControl(productCategory, Validators.required),
+      unit: new FormControl(productUnit, Validators.required),
       name: new FormControl(productName, Validators.required),
       price: new FormControl(productPrice, Validators.required),
       short_desc: new FormControl(productShortDesc, Validators.required),
       long_desc: new FormControl(productLongDesc, Validators.required)
     });
+  }
+
+  onGetListUnit(){
+    this.productService.getListUnitRequest()
+        .subscribe(data => {
+          console.log(data);
+          this.units = data;
+    })
+  }
+
+  onGetListCategory(){
+    this.productService.getListCategoryRequest()
+        .subscribe(data => {
+          console.log(data);
+          this.categories = data;
+    })
   }
 }
